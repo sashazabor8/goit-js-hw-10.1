d8c2c52955c611185f3df968fa3087bfeb1d84b0
sashazabor8 <106959082+sashazabor8@users.noreply.github.com>
2023-03-13 22:36:11 +0200

    добавляю остальную дз

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,22 @@
-import Player from '@vimeo/player'
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-    const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+    try {
+        onReboot()
+    } catch {
+        player.setCurrentTime(0)
+    }
 
-    player.on('timeupdate', function() {
-        console.log(seconds);
-    });
+    player.on('timeupdate', throttle(pressPlay,1000))
 
-  
+    function pressPlay(data) {
+        localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds))
+    }
+
+    function onReboot() {
+        player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+    }
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const throttle = require('lodash.throttle');
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('[name = "email"]'),
+    message: document.querySelector('[name = "message"]'),
+};
+let feedbackForm = {}
+
+try { 
+    refs.email.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+    refs.message.value = JSON.parse(localStorage.getItem('feedback-form-state')).message;
+    
+    feedbackForm = JSON.parse(localStorage.getItem('feedback-form-state'));
+} catch  {
+    refs.email.value = '';
+    refs.message.value = '';
+    
+}
+
+refs.form.addEventListener('input', throttle(addFedbackFormInLocalStorage,500))
+refs.form.addEventListener('submit', onSubmitForm)
+
+
+
+function addFedbackFormInLocalStorage (e) {
+    feedbackForm[e.target.name] = e.target.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackForm))
+}
+
+function onSubmitForm (e) {
+    e.preventDefault();
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    localStorage.removeItem('feedback-form-state');
+    refs.email.value = '';
+    refs.message.value = '';
+}
